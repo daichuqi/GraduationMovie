91a612485f8fede9abd56ae62d3586e2ef736826
chuqidai <daichuqi@gmail.com>
2015-09-22 17:22:41 -0400

    add video

--- build/app.js
+++ build/app.js
@@ -1,6 +1,15 @@
+var vid = document.getElementById("film");
+
 $('#play').click(function() {
-  var vid = document.getElementById("film");
   vid.play();
   vid.webkitEnterFullscreen();
   console.log('play')
 });
+
+
+window.onkeyup = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+   if (key == 27) {
+    vid.pause();
+   }
+}
